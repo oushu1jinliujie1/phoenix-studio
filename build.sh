6e97c5a1535e3b10408e360f39a6a0fa3773bb03
tangxy <tang.xiangyue@oushu.io>
2022-09-26 16:33:33 +0800

    编译脚本

---
+++ build.sh
@@ -0,0 +1,62 @@
+#! /usr/bin/env bash
+set -e
+
+basepath=$(cd `dirname $0`; pwd)
+
+print_info() {
+    echo -e "[\033[1;34mINFO\033[0m] $1"
+}
+
+print_err() {
+    echo -e "[\033[1;31mError\033[0m] $1"
+}
+
+start-phoenix-studio() {
+    print_info "start phoenix-studio frontend server"
+    cd $basepath/phoenix-studio
+    npm run serve
+    cd -
+}
+
+build-phoenix-studio() {
+    print_info "start build phoenix-studio"
+    cd $basepath/phoenix-studio
+    npm install
+    npm run build
+    cd -
+}
+
+usage() {
+    echo "build [options]"
+    echo "Options:"
+    echo "-s    start phoenix-studio server"
+    echo "-b    build-ui phoenix-studio"
+    echo "help  show usage information"
+}
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 0
+fi
+
+while [[ $# -gt 0 ]]; do
+    opt=$1
+    shift
+    case $opt in
+    -s)
+        COMMANDS+=("start-phoenix-studio")
+        ;;
+    -b)
+        COMMANDS+=("build-phoenix-studio")
+        ;;
+    help | *)
+        usage
+        exit 0
+        ;;
+    esac
+done
+
+for i in "${COMMANDS[@]}"
+do
+    $i
+done
